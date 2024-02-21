ce7ed68a8d64a3a9a89eda73617621a2f2b4f55f
KimHyoengJin <gudwls9966@naver.com>
2024-02-21 16:44:11 +0900

    1

---
+++ src/main/java/com/hyungjin/board/BoardApplication.java
@@ -0,0 +1,13 @@
+package com.hyungjin.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+}
